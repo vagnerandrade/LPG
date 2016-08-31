7238a57a6977e086e246b9c8a449971bb1a1a136
vagnerandrade <vagnerandrade10@gmail.com>
2016-08-31 20:23:20 -0300

    Add files via upload

---
+++ lista3pg10_ex2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fib (int a, int b){
+	int  x = 0, y = 1, auxi, i,n;
+	int fibo;
+	fibo = for (i= 0;i<n;i++){
+		auxi = x+y;
+		x = y;
+		y = auxi;
+		}
+		return(fibo);
+	}
+
+
+
+
+void (){
+	
+	
+	
+	
+	
+	}
